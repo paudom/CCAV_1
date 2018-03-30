2eb882129fa089f6e110fa14314b2cb7470b1bd5
mariagibert <mariia.sagi@gmail.com>
2018-03-30 09:33:42 +0200

    buffer modified

--- Code/ALS_Standard.m
+++ Code/ALS_Standard.m
@@ -1,4 +1,4 @@
-path_code='/Volumes/Productions/Alternas/UPC/4A/CCAV/CCAV_1/Code';
-path_mus='/Volumes/Productions/Alternas/UPC/4A/CCAV/CCAV_1/Musica';
+path_code='C:\Users\Maria\Documents\UNIVERSITAT\4A\CCAV\PROJ1\CCAV_1\Code';
+path_mus='C:\Users\Maria\Documents\UNIVERSITAT\4A\CCAV\PROJ1\CCAV_1\Musica';
 cd(path_code);
 ALS_Coder
--- Code/ALS_buffer.m
+++ Code/ALS_buffer.m
@@ -2,5 +2,5 @@ function [frame] = ALS_buffer(ori_sig,samples)
     right=buffer(ori_sig(:,1),samples);
     left=buffer(ori_sig(:,2),samples);
     diff=left-right;
-    frame=[right;left;diff];
+    frame=[right;left];
 end
